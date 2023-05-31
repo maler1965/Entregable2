5005efb63e7e0c236dd2305cab766b498d7b86ee
PCpedroCevallos <pevallos1965@gmail.com>
2023-05-31 09:31:39 -0500

    Se ajusto las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css'
 import Weather from './componets/Weather'
+
 import image1 from "/images/weather/bg1.jpg";
 import image2 from "/images/weather/bg2.jpg";
 import image3 from "/images/weather/bg3.jpg";
@@ -16,6 +17,7 @@ import image9 from "/images/weather/bg9.jpg";
 import image10 from "/images/weather/bg10.webp";
 import image11 from "/images/weather/bg11.jpg";
 import image12 from "/images/weather/bg12.jpg";
+/* */
 import Loader from './componets/Loader';
 
 
@@ -26,6 +28,33 @@ function App() {
 
   const [weatherInfo, setweatherInfo] = useState(null)
   const [randomImage, setRandomImage] = useState(null)
+
+
+
+  useEffect(() => {
+    if (weatherInfo) {
+      if (weatherInfo?.dt > weatherInfo?.sys.sunrise && weatherInfo?.dt > weatherInfo?.sys.sunset) {
+        const randomImageIndex = Math.floor(Math.random() * images2.length);
+        const randomImg = images2[randomImageIndex];
+        setRandomImage(randomImg);
+
+      }
+
+      if (weatherInfo?.dt < weatherInfo?.sys.sunrise && weatherInfo?.dt < weatherInfo?.sys.sunset) {
+        const randomImageIndex = Math.floor(Math.random() * images2.length);
+        const randomImg = images2[randomImageIndex];
+        setRandomImage(randomImg);
+
+      }
+
+      if (weatherInfo?.dt > weatherInfo?.sys.sunrise && weatherInfo?.dt < weatherInfo?.sys.sunset) {
+        const randomImageIndex = Math.floor(Math.random() * images1.length);
+        const randomImg = images1[randomImageIndex];
+        setRandomImage(randomImg);
+
+      }
+    }
+  }, [weatherInfo])
 
 
 
@@ -44,29 +73,6 @@ function App() {
     navigator.geolocation.getCurrentPosition(success)
   }, [])
 
-
-  useEffect(() => {
-    if (weatherInfo?.dt >= weatherInfo?.sys.sunrise && weatherInfo?.dt <= weatherInfo?.sys.sunset) {
-      const randomImageIndex = Math.floor(Math.random() * images1.length);
-      const randomImg = images1[randomImageIndex];
-      setRandomImage(randomImg);
-      console.log(true)
-
-    }
-    else {
-      const randomImageIndex = Math.floor(Math.random() * images2.length);
-      const randomImg = images2[randomImageIndex];
-      setRandomImage(randomImg);
-      console.log(false)
-    }
-    console.log(null)
-  }, [])
-
-
-  /*<Weather weatherInfo={weatherInfo} />
-   
-   */
-
   return (
     <main className="bg-black min-h-screen text-white flex justify-center items-center font-principal-font p-2  background-size: cover  background-position: center" style={{
       backgroundImage: `url(${randomImage})`,
@@ -75,7 +81,6 @@ function App() {
     }} >
 
       {weatherInfo ? <Weather weatherInfo={weatherInfo} /> : <Loader />}
-
 
 
     </main>
--- src/componets/Weather.jsx
+++ src/componets/Weather.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react"
 import axios from 'axios'
 
-
-
-
 const Weather = ({ weatherInfo }) => {
 
     var weatherInfo2 = ' '
@@ -17,6 +14,7 @@ const Weather = ({ weatherInfo }) => {
         weatherInfo2 = weatherInfo
     } else {
         weatherInfo2 = city
+
     }
 
     const handleChangeTemp = () => {
@@ -39,74 +37,75 @@ const Weather = ({ weatherInfo }) => {
             .catch((err) => console.log(err));
     };
 
-
     return (
-        <section className='text-center grid gap-6' >
-            <h2 className='font-bold  text-2xl'>{weatherInfo2?.name}, {weatherInfo2?.sys.country}</h2>
 
-            <section className='grid gap-4 sm:grid-cols-[1fr_auto]' style={{ color: 'green' }}>
+        <main >
+            <section className='text-center grid gap-6'  >
+                <h2 className='font-bold  text-2xl'>{weatherInfo2?.name}, {weatherInfo2?.sys.country}</h2>
 
-                {/*seccion arriba */}
-                <article className='bg-white/70 p-2 rounded-3xl grid grid-cols-2 items-center '>
+                <section className='grid gap-4 sm:grid-cols-[1fr_auto]' style={{ color: 'green' }}>
 
-                    <h3 className='col-span-2 capitalize'>{weatherInfo2?.weather[0].description}</h3>
+                    {/*seccion arriba */}
+                    <article className='bg-white/70 p-2 rounded-3xl grid grid-cols-2 items-center '>
 
-                    <span className='text-4xl'>{isCelsius ? `${((weatherInfo2?.main.temp) - 273.15).toFixed(1)} °C` : `${(((weatherInfo2?.main.temp) - 273.15) * (9 / 5) + 32).toFixed(1)} °F`}</span>
+                        <h3 className='col-span-2 capitalize'>{weatherInfo2?.weather[0].description}</h3>
 
-                    <div>
-                        <img src={`https://openweathermap.org/img/wn/${weatherInfo2?.weather[0].icon}@4x.png`} alt="" />
-                    </div>
+                        <span className='text-4xl'>{isCelsius ? `${((weatherInfo2?.main.temp) - 273.15).toFixed(1)} °C` : `${(((weatherInfo2?.main.temp) - 273.15) * (9 / 5) + 32).toFixed(1)} °F`}</span>
 
-                </article>
-
-                {/*seccion abajo */}
-                <section className='bg-white/70 p-2 rounded-3xl grid grid-cols-3 justify-items-center sm:grid-cols-1 sm:items-center'>
-
-                    <article className='flex gap-2 sm:items-center'>
                         <div>
-                            <img src="./images/wind.png" alt="" />
+                            <img src={`https://openweathermap.org/img/wn/${weatherInfo2?.weather[0].icon}@4x.png`} alt="" />
                         </div>
-                        <span>{weatherInfo2?.wind.speed}  m/s</span>
+
                     </article>
 
+                    {/*seccion abajo */}
+                    <section className='bg-white/70 p-2 rounded-3xl grid grid-cols-3 justify-items-center sm:grid-cols-1 sm:items-center'>
 
-                    <article className='flex  sm:items-center'>
-                        <div className="pr-5" >
-                            <img src="./images/humidity.png" alt="" />
-                        </div>
-                        <span>{weatherInfo2?.main.humidity}  %</span>
-                    </article>
+                        <article className='flex gap-2 sm:items-center'>
+                            <div>
+                                <img src="./images/wind.png" alt="" />
+                            </div>
+                            <span>{weatherInfo2?.wind.speed}  m/s</span>
+                        </article>
 
 
-                    <article className='flex gap-2 sm:items-center'>
-                        <div>
-                            <img src="./images/pressure.png" alt="" />
-                        </div>
-                        <span>{weatherInfo2?.main.pressure}  hPa</span>
-                    </article>
+                        <article className='flex  sm:items-center'>
+                            <div className="pr-5" >
+                                <img src="./images/humidity.png" alt="" />
+                            </div>
+                            <span>{weatherInfo2?.main.humidity}  %</span>
+                        </article>
+
+
+                        <article className='flex gap-2 sm:items-center'>
+                            <div>
+                                <img src="./images/pressure.png" alt="" />
+                            </div>
+                            <span>{weatherInfo2?.main.pressure}  hPa</span>
+                        </article>
+
+                    </section>
 
                 </section>
 
+                <div className=" flex gap-2"  >
+                    <form onSubmit={handleSubmit} className="max-w-max mx-auto">
+                        <div className="flex rounded-md overflow-hidden ">
+                            <input
+                                id="name"
+                                type="text"
+                                placeholder="Write your city..."
+                                className="text-black outline-none px-2"
+                            />
+                            <button className="bg-red-500 p-2"> Search by City</button>
+                        </div>
+                    </form>
+
+                    <button onClick={handleChangeTemp} className="bg-white/70 p-2  rounded-3xl" style={{ color: 'green' }} >Change F/C</button>
+                </div>
+
             </section>
-
-            <div className=" flex gap-2"  >
-                <form onSubmit={handleSubmit} className="max-w-max mx-auto">
-                    <div className="flex rounded-md overflow-hidden ">
-                        <input
-                            id="name"
-                            type="text"
-                            placeholder="Write your city..."
-                            className="text-black outline-none px-2"
-                        />
-                        <button className="bg-red-500 p-2"> Search by City</button>
-                    </div>
-                </form>
-
-                <button onClick={handleChangeTemp} className="bg-white/70 p-2  rounded-3xl" style={{ color: 'green' }} >Change F/C</button>
-            </div>
-
-        </section>
-
+        </main>
 
     )
 }
